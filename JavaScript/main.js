155eb1c158ed34d968af797bdef62f2d2a9ffee5
Alissa Jackson <alissa.j636@nebo.local>
2022-09-08 11:38:51 -0600

    Everything

---
+++ JavaScript/main.js
@@ -0,0 +1,11 @@
+function myFunction(){
+  document.getElementById("attempt").innerHTML="Hello, isnt this fun?";
+  document.getElementById("attempt2").innerHTML="I find it kinda fun!"
+}
+console.log(18+6);
+
+let a,b,c;
+a=10;c=36; b=a-c;
+document.getElementById("statements").innerHTML="The value of b is "+ b +".";
+
+/*alert("Popup output!")*/
